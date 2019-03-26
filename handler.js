442d79a9dfdc522832c1e632ae9b090624325381
Alexander Smith <acsmith.dev@gmail.com>
2019-03-26 13:44:11 -0400

    extract data from s3 events

--- handler.js
+++ handler.js
@@ -2,14 +2,15 @@
 
 module.exports.s3_notification = async (event) => {
   // Extract S3 data from 'event' object
+  const uploadData = event.Records.map(record => {
+    return {
+      bucketName: record.s3.bucket.name,
+      file: record.s3.object.key,
+      fileSize: record.s3.object.size
+    }
+  })[0];
+  
   // Generate E-mail with Extracted data
   // Send E-mail
   // Return Response
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
 };
